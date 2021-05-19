a0867ea2fc3428ce1bfed092e89cf1c19ce896cc
ga-felix <44716827+ga-felix@users.noreply.github.com>
2021-05-19 00:08:32 -0300

    Create Queue and BFS

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-start cmd.exe /k "cd %~dp0 & gcc -c graphopmatrixdi.c & gcc -c main.c & gcc -o runmatrix.exe graphopmatrixdi.o main.o & runmatrix > debug.txt"
+start cmd.exe /k "cd %~dp0 & gcc -c queue.c & gcc -c graphopmatrixdi.c & gcc -c main.c & gcc -o runmatrix.exe queue.o graphopmatrixdi.o main.o & runmatrix > debug.txt"
